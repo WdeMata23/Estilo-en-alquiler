cebb9612ac95d795ad39a0a55fac5602f3568147
WdeMata23 <148029002+WdeMata23@users.noreply.github.com>
2024-08-28 16:11:09 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function filtrarProductos(categoria) {
     var productos = document.getElementsByClassName('producto');
-    
+
     for (var i = 0; i < productos.length; i++) {
-        if (categoria === 'todos') {
+        if (productos[i].classList.contains(categoria)) {
             productos[i].style.display = 'block';
         } else {
-            if (productos[i].classList.contains(categoria)) {
-                productos[i].style.display = 'block';
-            } else {
-                productos[i].style.display = 'none';
-            }
+            productos[i].style.display = 'none';
         }
     }
 }
